630c417cfbc996efa47b72b24e82455a9171167b
dejavuapt <thepoli.b@gmail.com>
2022-10-10 13:15:44 +0500

    simple example.data

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 import chalk from 'chalk';
 import cheerio from 'cheerio';
+import { PassThrough } from 'stream';
 import { slugify } from 'transliteration';
 
 import { arrayFromLenghts } from './helpers/common.js';
@@ -26,18 +27,30 @@ const pages = 1;
             const mobileItems = []
 
             $('.ProductCardHorizontal').each((i, header) => {
-                const title = $(header).children('.ProductCardHorizontal__header-block').text()
+                const title = $(header).attr('data-params')
+                const count_opinions = $(header).attr('data-opinion')
+                var parsTitle = JSON.parse(title)
 
                 mobileItems.push({
-                    title,
-
+                    categoryid: parsTitle['categoryId'],
+                    title: parsTitle['shortName'],
+                    copy_title: slugify(parsTitle['shortName']),
+                    brand: parsTitle['brandName'],
+                    price: parsTitle['price'],
+                    rating: 0,
+                    opinions_size: count_opinions,
+                    url_opinions: chd_title.children().first().attr('href')
                 })
+            })
+            $('.ProductCardHorizontal__rating-block').each((i, header) => {
+                const url_op = $(header).children().first().attr('href')
+                
             })
             console.log(mobileItems)
         }
     }
     catch (err){
-        console.log(red('An error has catched \n'));
+        console.log(chalk.red('An error has catched \n'));
         console.log(err);
     }
 })()
