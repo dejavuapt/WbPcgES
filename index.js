f818b205168267e740a81d5a688e40ac399ef759
dejavuapt <thepoli.b@gmail.com>
2022-11-01 10:52:35 +0500

    new options: choose a site

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ import listItemsHandlerCitilink from './handlers/listItemsHandlerCitilink.js';
 import { arrayFromLenghts } from './helpers/common.js';
 import { getPageContent } from './helpers/puppeteer.js';
 
-const SITE = 'https://www.citilink.ru/catalog/smartfony/?p='
-const pages = process.argv[2] || 1
+const sites_url = [{name: 'citilink', url: 'https://www.citilink.ru/catalog/smartfony/?p='}, {name: 'dns', url: 'https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?p='}]
+
+let SITE = sites_url[0].url
+sites_url.forEach((site) => {
+    if(site.name == process.argv[2].toLowerCase()){
+        SITE = site.url;
+    }
+})
+const pages = process.argv[3] || 1
 
 console.log(pages);
 
@@ -21,9 +28,9 @@ console.log(pages);
         for(const page of arrayFromLenghts(pages)){
             var url = SITE.concat(page.toString())
             console.log(url);
-            const pageContent = await getPageContent(url)
-            const mobiles = eachForCitilinkSite(pageContent)
-            await listItemsHandlerCitilink(mobiles)
+            // const pageContent = await getPageContent(url)
+            // const mobiles = eachForCitilinkSite(pageContent)
+            // await listItemsHandlerCitilink(mobiles)
         }
     }
     catch (err){
